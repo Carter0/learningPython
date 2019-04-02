de48ed610a1065cb0cde444303abe520ed103cec
Carter Weinberg <weinbergcarter@gmail.com>
2019-04-02 11:26:07 -0400

    Arg parse is proving difficult to learn

--- reverseList.py
+++ reverseList.py
@@ -1,13 +1,12 @@
 #!/usr/local/bin/python3
 
-import sysparse
+import argparse
 
 # Reverse a list of whatever without using some kind of reverse list function
 # Just starting working in python, thought this was a good introduction to programming in the language
 # Also introduction to parsing command line arguments in python
 
 
-starting_list = []
 reversed_list = []
 
 
@@ -17,8 +16,15 @@ def reverse_list():
 		reversed_list.append(starting_list.pop())
 
 
+def main(): 
+	parser = argparse.ArgumentParser()
+	parse.add_argument("list", nargs='*', help='Enter all the items you want to reverse.')
+	starting_list = parse.parse_args()
+	reversed_list()
+
+
 if __name__ == '__main__':
-	#reversed_list()
+	main()
 
 
 
