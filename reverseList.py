d3f60ef454faba3ad17fd2fbdb4d9770133fbe20
Carter Weinberg <weinbergcarter@gmail.com>
2019-04-02 14:56:15 -0400

    Finished

--- reverseList.py
+++ reverseList.py
@@ -4,36 +4,21 @@ import argparse
 
 # Reverse a list of whatever without using some kind of reverse list function
 # Just starting working in python, thought this was a good introduction to programming in the language
-# Also introduction to parsing command line arguments in python
+# Also introduction to parsing command line arguments in python, which proved to be the far more time consuming task.
 
-
-reversed_list = []
-
-
-
-def reverse_list():
+def reverse_list(starting_list):
+	reversed_list = []
 	while len(starting_list) != 0 :
 		reversed_list.append(starting_list.pop())
+	return reversed_list
 
 
 def main(): 
 	parse = argparse.ArgumentParser()
-	parse.add_argument("list", nargs='*', help='Enter all the items you want to reverse.')
-	starting_list = parse.parse_args()
-	print(starting_list)
-	#reversed_list()
+	parse.add_argument("starting_list", nargs='*', help='Enter all the items you want to reverse.')
+	args = parse.parse_args()
+	print(reverse_list(args.starting_list))
 
 
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-
-
-
-
-
-#print(reversed_list)
