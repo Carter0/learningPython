59d4d7d38b0d3dee70b1f1d787afde1255e50232
Carter Weinberg <weinbergcarter@gmail.com>
2019-04-02 10:06:36 -0400

    Got reverse working, need to get argparse working

--- reverseList.py
+++ reverseList.py
@@ -1,2 +1,32 @@
+#!/usr/local/bin/python3
+
+import sysparse
+
 # Reverse a list of whatever without using some kind of reverse list function
 # Just starting working in python, thought this was a good introduction to programming in the language
+# Also introduction to parsing command line arguments in python
+
+
+starting_list = []
+reversed_list = []
+
+
+
+def reverse_list():
+	while len(starting_list) != 0 :
+		reversed_list.append(starting_list.pop())
+
+
+if __name__ == '__main__':
+	#reversed_list()
+
+
+
+
+
+
+
+
+
+
+#print(reversed_list)
