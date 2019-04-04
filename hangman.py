5b6019951fa07d9538fa163cdf8285ffdf02287a
Carter Weinberg <weinbergcarter@gmail.com>
2019-04-04 16:24:03 -0400

    A good midway point

--- hangman.py
+++ hangman.py
@@ -1,34 +1,70 @@
 #!/usr/local/bin/python3
+import argparse
 
-# Decided to try making hangman, thought it could be fun before I get to some web stuff. 
-# No front end or GUI for this. Just input word and then guess word. 
+# Decided to try making hangman, thought it could be fun before I get to some web stuff.
+# No front end or GUI for this. Just input word and then guess word.
 
 
-# The word the user has to guess. 
-word = ['t', 'e', 's', 't', 'i', 'n', 'g']
-lives = 5
-game_over = False
 
-def guess_word(char):
+
+def guess_char(char, word, lives):
 	if char in word:
-		# Do guess correct stuff. 
-	else: 
-		# Do guess incorrect stuff 
+		remove_char(char)
+	else:
+		lose_life()
 
-def lose_life():
+def lose_life(lives):
 	lives = lives - 1;
+	# Every time you lose a life do a check to see if you are out of lives.
+	if lives == 0:
+		game_over = True
 
-# Remove the char in the word as long as that character exists in it
 def remove_char(char):
 	while char in word:
 		word.remove(char)
 
-def main():
+def ask_input():
+	char = ""
+	while True:
+		print("Enter a char to print: ")
+		char = input()
+		if char == len(char) * char[0] and char.isdigit() != True:
+			print("The char you wrote is: " + char)
+			return char
+		else:
+			print("You can only enter a char")
+
+def game_setup(word, lives):
+
+	game_over = False
+
 	# Checking if the word is empty
 	if not word:
 		game_over = True
-	while game_over == False: 
-		guess_word()
+	while game_over == False:
+		print(word)
+		char = ask_input()
+		guess_char(char)
+
+
+def main():
+	parser = argparse.ArgumentParser(description= 'Program runs a game of hangman.')
+	parser.add_argument('word', help='Enter the word you want to play with.')
+	parser.add_argument('lives', type=int, help='Enter the number of lives you want to play with.')
+	args = parser.parse_args()
+
+	game_setup(args.word, args.lives)
+
+
+
+if __name__ == '__main__':
+	main()
+
+# Add in the variables above as arguments to the program. I think that is way forward.
+
+
+
+
 
 
 
