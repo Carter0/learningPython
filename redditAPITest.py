0834ae341167348bacacdce4e6632d15982a1cfe
Carter Weinberg <weinbergcarter@gmail.com>
2019-04-26 10:09:20 -0400

    Still working on my scripts

--- redditAPITest.py
+++ redditAPITest.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import praw
+from praw.models import MoreComments
 
 # Thought I really didnt understand what I was touching with PRAW and that I needed to go over some simple examples, so that is what I am doing.
 # Need to set up OAuth 2 certification so I can use reddit API. https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps
@@ -27,17 +28,42 @@ for submission in subreddit.hot(limit=1):
     print(submission.url)
 """
 
-
+"""
 subreddit = reddit.subreddit('sekiro')
 posts = [posts for posts in subreddit.top('all', limit=10)]
 comments = [comments for comments in posts[0].comments]
 
 
+# Learning about comment forests!
+# They are a list of top level comments with a comment forest of replies.
 
+"
+# prints all the top-level-comments in the first post in all time top
+# at least it does until eventually I need to reset the comments cause it runs out.
 print(posts[0].title)
 print('\n')
 for comment in comments:
+    # One way of dealing with morecomments
+    if isinstance(comment, MoreComments):
+        continue
     print(comment.body)
+"""
+
+# Another way of dealing with more comments, better it seems.
+
+submission = reddit.submission(url='https://www.reddit.com/r/programming/comments/bh67bz/raycasting_engine_in_factorio_vanilla_017/')
+#submission.comments.replace_more(limit=None)
+#for top_level_comment in submission.comments:
+#     for second_level_comment in top_level_comment.replies:
+#        print(second_level_comment.body)
+
+# This will output all the front level comments, then all the second level comments, then all the third etc.
+submission.comments.replace_more(limit=None)
+for comment in submission.comments.list():
+    print(comment.body)
+
+
+
 
 
 
--- redditQuery.py
+++ redditQuery.py
@@ -1,7 +1,36 @@
 #!/usr/local/bin/python3
-import requests
+import praw
+
+
+
+reddit = praw.Reddit('Carter', user_agent='My Program')
 
 # Script hits the reddit api endpoint to see which comments have gotten above a certain likes threshold for my user
 # Might just use praw cause it makes things really easy with Python
 
+# Might want to split this into a few separate scripts, for sure a few separate functions
+# start at bottom, just make a script that looks through each post for comments that have above a certain amount of likes, for now we might just do top level comments, current score to check is 5000
+# Then go above that and make a scrip that searches each subreddit for top few posts for the day
+# Then make a script that goes through all the subreddits you are subscribed to
 
+
+
+
+# Print all the top level comments that got above the upvote requirment in that submission.
+def get_comments(submission, upvote_requirement):
+    for top_level_comment in submission.comments:
+        if top_level_comment.score >= upvote_requirement:
+            print(top_level_comment.body)
+            print('------------------------------------------------------')
+
+# Now we are dealing with posts instead of comments
+subreddit = reddit.subreddit('AskReddit')
+for submission in subreddit.hot(limit=10):
+    submission.comments.replace_more(limit=0)
+    print(submission.title)
+    print(submission.author)
+    get_comments(submission, 10000)
+    print('===============================================')
+
+
+#get_comments(submission, 4000)
