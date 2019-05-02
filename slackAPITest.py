e4d2eb6c8daf084d890e017246793b30f58f6ad3
Carter Weinberg <weinbergcarter@gmail.com>
2019-05-02 15:09:43 -0400

    The many different ways to post something to a slack channel with Python

--- slackAPITest.py
+++ slackAPITest.py
@@ -3,33 +3,57 @@
 import requests
 import json
 import os
-
-# Messing around with the slack api now. Trying to make a simple bot.
-
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+import datetime
+from slackclient import SlackClient
 
 
-# Need a way to grab the specific info I am looking for
+# Messing around with the slack api now. Trying to make a simple bot that prints the time.
+# This method works well, but it lacks the functionality of other methods.
+# Runs with a chron job.
+# The cron job I used to test it: */5 * * * * ~/Desktop/learningPython/slackAPITest.py
+
 """
-url = 'https://slack.com/api/conversations.list'
-params = {'token': SLACK_BOT_TOKEN}
+webhook_url = 'https://hooks.slack.com/services/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'
+now = datetime.datetime.now()
+time = 'The current time is {}:{}'.format(now.hour, now.minute)
 headers = {'content-type': 'application/json'}
-response = requests.get(url, params=params, headers=headers)
+payload = {'text': time}
+response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
+"""
+
+# I want to now try doing it another way. This way provides a warning about the content-type being wrong. But changing it also provides errors.
+# I do not like this method.
+"""
+url = 'https://slack.com/api/chat.postMessage'
+headers = {'Content-type': 'application/json', 'Authorization': '<GET THE TOKEN LATER>'}
+data = {'channel': 'CJ9S864SG', 'text': 'It is a warning not an error.'}
+response = requests.post(url, data=json.dumps(data), headers=headers)
 print(response.text)
 """
 
+# Now lets use the python wrapper
+# This is my favorite way to do it.
+"""
+slack_token = os.environ["SLACK_BOT_TOKEN"]
+sc = SlackClient(slack_token)
 
+sc.api_call(
+  "chat.postMessage",
+  channel="CJ9S864SG",
+  text="This is the python wrapper way :)"
+)
+"""
 
-webhook_url = 'https://hooks.slack.com/services/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'
+# Now trying to schedule it
+slack_token = os.environ["SLACK_BOT_TOKEN"]
+sc = SlackClient(slack_token)
 
-
-# You have to retrieve the token as an environment variable, make sure to put it in.
-# Found a good way of doing it with a webhook url set up for this app.
-headers = {'content-type': 'application/json'}
-payload = {'text': 'Hello, world'}
-response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
-
-print(response)
+sc.api_call(
+  "chat.scheduleMessage",
+  channel="CJ9S864SG",
+  text="This is a timed message! :):)",
+  post_at="1556824221"
+)
 
 
 
